15798566fc61bd73138e018be73ec7d823639c63
leejeawon92 <jeawon33333@gmail.com>
2022-11-25 21:40:30 +0900

    vReact: status-bar

--- kokoaclonereact/src/App.js
+++ kokoaclonereact/src/App.js
@@ -1,9 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import styled from "styled-components";
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import { faWifi ,faBolt, faBattery } from '@fortawesome/free-solid-svg-icons'
+
+const StatusBar = styled.div`
+  width: 33%;
+  top: 0px;
+  position: fixed;
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  padding: 5px 3px;
+  background-color: white;
+`
+
+const LeftColumn = styled.div`
+  width: 33%;
+  span {
+    margin-right: 5px;
+  }
+`
+
+const CenterColumn = styled.div`
+  width: 33%;
+  display: flex;
+  justify-content: center;
+`
+
+const RightColumn = styled.div`
+  width: 33%;
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
+  padding: 0 15px;
+`
+
 
 function App() {
   return (
     <div className="App">
+      <StatusBar>
+        <LeftColumn>
+          <span>No Service</span>
+          <FontAwesomeIcon icon={faWifi} />
+        </LeftColumn>
+
+        <CenterColumn>
+          <span>17:00</span>
+        </CenterColumn>
+
+        <RightColumn>
+          <span>110%</span>
+          <FontAwesomeIcon icon={faBolt} />
+          <FontAwesomeIcon icon={faBattery} />
+        </RightColumn>
+      </StatusBar>
+      
     </div>
   );
 }
